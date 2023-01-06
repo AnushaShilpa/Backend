a53b44416aa3edc2c4f575adccafa6452aa6339e
AnushaShilpa <anushanataraj01@gmail.com>
2023-01-06 23:06:29 +0530

    sending response(html page)

--- new.js
+++ new.js
@@ -1,10 +1,22 @@
+
 const http = require('http');
+const fs = require('fs')
 
 const server=http.createServer((req ,res)=>
 {
-  res.write('Anusha');
-  res.end();
-  
+    res.writeHead(200,{'content-Type':'text/html'})
+    fs.readFile('new.html',function(error ,data)
+    {
+      if(error)
+      {
+        res.writeHead(404)
+        res.write("Error:File not found")
+      }
+      else{
+        res.write(data)
+      }
+      res.end()
+    })
 });
 
 server.listen(4000);
