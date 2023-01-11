1c60be962986645f03efd8afa2a9b417857fd1eb
AnushaShilpa <anushanataraj01@gmail.com>
2023-01-11 23:05:47 +0530

    using body parser

--- new1.js
+++ new1.js
@@ -1,17 +1,23 @@
 
 const express =require('express');
 const app = express();
+const bodyparser=require('body-parser');
+
 const port =4000
 
-app.use((req,res,next)=>{
-  res.send('<h1>Hello Welcome To Express</h1>')
-  console.log("i am middle ware")
-  next();
-  
+app.use(bodyparser.urlencoded({executable:false}))
+app.use('/add-product',(req,res,next)=>
+{
+  res.send('<form action="/product" method="POST"><input type="text" name="title"><button type="submit">Add product</button></form>')
 })
-app.use((req,res,next)=>{
-  console.log('hard work leads to sucess')
-  
+app.post('/product',(req,res,next)=>
+{
+  console.log(req.body);
+  res.redirect('/');
 })
-// const server= http.createServer(app);
+app.use('/',(req,res,next)=>
+{
+  res.send('<h1>Wecolme to Node Js</h1>');
+})
+
 app.listen(port)
