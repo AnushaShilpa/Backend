f6762377618b48451cf5e419eb359419b65a5aa4
AnushaShilpa <anushanataraj01@gmail.com>
2023-01-12 14:43:35 +0530

    routes used fr developing

--- new1.js
+++ new1.js
@@ -3,25 +3,18 @@ const express =require('express');
 const app = express();
 const bodyparser=require('body-parser');
 
+const adminproduct =require('./routes/admin');
+const shopproduct=require('./routes/shop');
+
 const port =4000
 
 app.use(bodyparser.urlencoded({executable:false}))
-app.use('/add-product',(req,res,next)=>
-{
-  
-  res.send('<form action="/product" method="POST"><input type="text" name="title"/> <input type="number" name="size"/><button type="submit">Add product</button></form>')
-  
+app.use(adminproduct);
+app.use(shopproduct);
 
-});
-
-app.use('/product',(req,res,next)=>
+app.use((req,res,next)=>
 {
-  console.log(req.body);
-  res.redirect('/');
-})
-app.use('/',(req,res,next)=>
-{
-  res.send('<h1>Wecolme to Node Js</h1>');
+   res.status(404).send('<h1>404 Error Page not found</h1>')
 })
 
 app.listen(port)
